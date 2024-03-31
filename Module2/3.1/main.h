6ce53f3c1a390735cd37e06937cc0a599c4d578a
novosyolov <powerkja@gmail.com>
2024-03-31 14:10:38 +0700

    Complete 3.1(1)

--- Module2/3.1/main.h
+++ Module2/3.1/main.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h> 
+#include <string.h>
 
+void mask();
+void print_bits(int a);
+int number_mask(char* mask);
+int letter_mask(char* u, int* a, int num);
+void set_bit(int *number, int pos_bit, short bit);
+int get_bit(int bit, int pos_bit);
 
 #endif // MAIN_H
--- Module2/3.1/mask.c
+++ Module2/3.1/mask.c
@@ -1,31 +1,46 @@
 #include "main.h"
-#include <string.h>
+
+int main(){
+  mask();
+  return 0;
+}
 
 void mask(){
-    char access_u[3];
-    char access_g[3];
-    char access_o[3];
+    char access_u[4];
+    char access_g[4];
+    char access_o[4];
     int result = 0;
-    printf("Введите права доступа в буквенном или цифровом обозначении (например, rwx или 111): ");
+    printf("Введите права доступа в буквенном обозначении (rwx-) для пользователя,\n");
+    printf("Или цифровом обозначении (777) для всех: \n");
     fgets(access_u,4,stdin);
-    printf("gdfgsdf%s\n",access_u);
     if (access_u[0]<'8' && access_u[0]>='0'){
         result = number_mask(access_u);
-        printf("gdfgsdf%s\n",access_u);
     }
     else{
+        getchar();//перехват \n из потока после fgets
+        printf("Введите права доступа в буквенном обозначении для группы: ");
         fgets(access_g,4,stdin);
+        getchar();//перехват \n из потока после fgets
+        printf("Введите права доступа в буквенном обозначении для остальных: ");
         fgets(access_o,4,stdin);
-        letter_mask(access_u,&result,0);
+        letter_mask(access_u,&result,6);
         letter_mask(access_g,&result,3);
-        letter_mask(access_o,&result,6);
+        letter_mask(access_o,&result,0);
     }
- // print_bits(result);
+ printf("Битовое представление прав доступа:\n");
+ print_bits(result);
 }
 
 void print_bits(int a){
-  for(int i = 0; i < 9; i++){
+  char* person[3]={"user","group","other"};
+  int n = 0;
+
+  for(int i = 8; i >= 0; i--){
     printf("%d ",(a>>i)&1);
+    if((i%3) == 0){
+      printf("%s\n",person[n]);
+      n++;
+    }
   }
 }
 
@@ -35,13 +50,13 @@ int number_mask(char* mask){
 
   for (int i = 0; i < 3; i++){
     ugo[i] = *(mask+i)-'0';
-    printf("%d\n",ugo[i]);
   }
 
 for(int j = 0; j < 3;j++){
   for (int i = 0; i < 3; i++){
-    int bit = get_bit(ugo[j],i*(j+1));
-    set_bit(res,i*(j+1),bit);
+    int bit = get_bit(ugo[j],i+1);
+    res=res<<1;
+    if(bit)res|=1;
   }
 }
   return res;
@@ -52,13 +67,13 @@ int letter_mask(char* u,int* a,int num){
         for (int i = 0; i < 3; i++) {
         switch (*(u+i)) {
             case 'r':
-                set_bit(result,3+num,1); // установить бит для чтения
+                set_bit(a,3+num,1); // установить бит для чтения
                 break;
             case 'w':
-                set_bit(result,2+num,1); // установить бит для записи
+                set_bit(a,2+num,1); // установить бит для записи
                 break;
             case 'x':
-                set_bit(result,1+num,1); // установить бит для выполнения
+                set_bit(a,1+num,1); // установить бит для выполнения
                 break;
             default:
                 break;
@@ -67,22 +82,17 @@ int letter_mask(char* u,int* a,int num){
     return result;
 }
 
-void set_bit(int *bit, int pos_bit, short num_zero_or_one) {
-  int digits_32[32] = {0};
+void set_bit(int *number, int pos_bit, short bit) {
+  int bits_32[32] = {0};
   for (int i = 0; i < 32; i++) {
-    digits_32[i] = 1 << i;
+    bits_32[i] = 1 << i;
   }
-  if (num_zero_or_one)
-    *bit |= digits_32[pos_bit - 1];
+  if (bit)
+    *number |= bits_32[pos_bit - 1];
   else
-    *bit &= ~digits_32[pos_bit - 1];
+    *number &= ~bits_32[pos_bit - 1];
 }
 
 int get_bit(int bit, int pos_bit) {
   return (bit >> (--pos_bit)) & 1;
-}
-
-int main(){
-  mask();
-  return 0;
 }
